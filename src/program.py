38156c21873879cc5980eb8b7653725d4aee2c2f
Henrik Christensen <hdc@mjolner.dk>
2023-09-06 16:24:27 +0200

    make functions into method in an class

--- src/program.py
+++ src/program.py
@@ -1,47 +1,39 @@
-from func.validation import validation
-import numpy as np
+from knn import KNN
 
 
 def main():
-    # Same labels will be used for all the experiments
-    labels_file_path = "data/MNIST-5-6-Subset-Labels.txt"
-    labels = np.loadtxt(labels_file_path)
-    # Convert labels from {5, 6} to {-1, 1}
-    labels = np.where(labels == 5, -1, 1)
-
-    # Same number of validation sets for all experiments
-    num_validation_sets = 5
-
-    # Same number of training points for all experiments
-    m = 50
-
-    # Base data experiment
-    base_data_file_path = "data/MNIST-5-6-Subset.txt"
-    base_output_path = "output/base"
-    base_data = np.loadtxt(base_data_file_path).reshape(1877, 784)
-    n_values = [10, 20, 40, 80]  # Size of validation set
-    validation(base_data, labels, m, n_values, num_validation_sets, base_output_path)
+    uncorrupted = KNN(name="Uncorrupted", data_file_path="data/MNIST-5-6-Subset.txt", output_path="output/uncorrupted", n_values=[10, 20, 40, 80])
+    uncorrupted.validation()
 
     # Light corruption experiment
-    light_corruption_data_file_path = "data/MNIST-5-6-Subset-Light-Corruption.txt"
-    light_corruption_output_path = "output/light_corruption"
-    light_corruption_data = np.loadtxt(light_corruption_data_file_path).reshape(1877, 784)
-    n_values = [80]
-    validation(light_corruption_data, labels, m, n_values, num_validation_sets, light_corruption_output_path)
+    light_corruption = KNN(
+        name="Light-Corruption",
+        data_file_path="data/MNIST-5-6-Subset-Light-Corruption.txt",
+        output_path="output/light_corruption",
+        n_values=[80],
+        skip_variance=True,
+    )
+    light_corruption.validation()
 
     # Moderate corruption experiment
-    moderate_corruption_data_file_path = "data/MNIST-5-6-Subset-Moderate-Corruption.txt"
-    moderate_corruption_output_path = "output/moderate_corruption"
-    moderate_corruption_data = np.loadtxt(moderate_corruption_data_file_path).reshape(1877, 784)
-    n_values = [80]
-    validation(moderate_corruption_data, labels, m, n_values, num_validation_sets, moderate_corruption_output_path)
+    moderate_corruption = KNN(
+        name="Moderate-Corruption",
+        data_file_path="data/MNIST-5-6-Subset-Moderate-Corruption.txt",
+        output_path="output/moderate_corruption",
+        n_values=[80],
+        skip_variance=True,
+    )
+    moderate_corruption.validation()
 
     # Heavy corruption experiment
-    heavy_corruption_data_file_path = "data/MNIST-5-6-Subset-Heavy-Corruption.txt"
-    heavy_corruption_output_path = "output/heavy_corruption"
-    heavy_corruption_data = np.loadtxt(heavy_corruption_data_file_path).reshape(1877, 784)
-    n_values = [80]
-    validation(heavy_corruption_data, labels, m, n_values, num_validation_sets, heavy_corruption_output_path)
+    heavy_corruption = KNN(
+        name="Heavy-Corruption",
+        data_file_path="data/MNIST-5-6-Subset-Heavy-Corruption.txt",
+        output_path="output/heavy_corruption",
+        n_values=[80],
+        skip_variance=True,
+    )
+    heavy_corruption.validation()
 
 
 if __name__ == "__main__":
