37a97e33db46b74625d9ea725f59f06b450870af
Henrik Christensen <hdc@mjolner.dk>
2023-10-16 15:51:19 +0200

    restructure

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ from knn import KNN
 
 
 def main():
-    uncorrupted = KNN(name="Uncorrupted", data_file_path="data/MNIST-5-6-Subset.txt", output_path="output/uncorrupted", n_values=[10, 20, 40, 80])
+    uncorrupted = KNN(name="Uncorrupted", data_file_path="data/MNIST-5-6-Subset.txt", output_path="images/uncorrupted", n_values=[10, 20, 40, 80])
     uncorrupted.validation()
 
     # Light corruption experiment
     light_corruption = KNN(
         name="Light-Corruption",
         data_file_path="data/MNIST-5-6-Subset-Light-Corruption.txt",
-        output_path="output/light_corruption",
+        output_path="images/light_corruption",
         n_values=[80],
         skip_variance=True,
     )
@@ -19,7 +19,7 @@ def main():
     moderate_corruption = KNN(
         name="Moderate-Corruption",
         data_file_path="data/MNIST-5-6-Subset-Moderate-Corruption.txt",
-        output_path="output/moderate_corruption",
+        output_path="images/moderate_corruption",
         n_values=[80],
         skip_variance=True,
     )
@@ -29,7 +29,7 @@ def main():
     heavy_corruption = KNN(
         name="Heavy-Corruption",
         data_file_path="data/MNIST-5-6-Subset-Heavy-Corruption.txt",
-        output_path="output/heavy_corruption",
+        output_path="images/heavy_corruption",
         n_values=[80],
         skip_variance=True,
     )
